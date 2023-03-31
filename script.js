c5d5f085711105cc616b3f6ec7284825b268527f
Abyl239 <altajanovabylay@gmail.com>
2023-04-01 05:14:27 +0600

    testing

---
+++ script.js
@@ -0,0 +1,55 @@
+const gap = 9;
+
+const carousel = document.getElementById("carousel"),
+    content = document.getElementById("content"),
+    next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+    carousel.scrollBy(width, 0);
+    if (carousel.scrollWidth !== 0) {
+        prev.style.display = "flex";
+    }
+    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "none";
+    }
+});
+prev.addEventListener("click", e => {
+    carousel.scrollBy(-(width), 0);
+    if (carousel.scrollLeft - width - gap <= 0) {
+        prev.style.display = "none";
+    }
+    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "flex";
+    }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".container");
+    let header = document.querySelector("header");
+    let welcome = document.querySelector("#welcome");
+
+    if(welcome.getBoundingClientRect().top<-10){
+        header.classList.add("scrolled")
+    }
+    else{
+        header.classList.remove("scrolled")
+    }
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+reveal()
+window.addEventListener("scroll", reveal);
